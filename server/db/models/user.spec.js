d9b970096b29b4c26f2cd831cb0b92011ec2f751
Brittany Hill <brit.hill10@gmail.com>
2018-10-30 09:43:39 -0500

    Add User model

--- server/db/models/user.spec.js
+++ server/db/models/user.spec.js
@@ -1,32 +1,35 @@
 /* global describe beforeEach it */
 
-const {expect} = require('chai')
+const { expect } = require('chai')
 const db = require('../index')
 const User = db.model('user')
 
 describe('User model', () => {
   beforeEach(() => {
-    return db.sync({force: true})
+    return db.sync({ force: true })
   })
 
-  describe('instanceMethods', () => {
-    describe('correctPassword', () => {
-      let cody
+  describe('valid email', () => {
+    let cody
 
-      beforeEach(async () => {
-        cody = await User.create({
-          email: 'cody@puppybook.com',
-          password: 'bones'
-        })
+    beforeEach(async () => {
+      cody = await User.create({
+        email: 'cody@puppybook.com',
       })
+    })
 
-      it('returns true if the password is correct', () => {
-        expect(cody.correctPassword('bones')).to.be.equal(true)
-      })
+    it('returns true if the email is valid', () => {
+      expect(cody.email).to.equal('cody@puppybook.com')
+    })
 
-      it('returns false if the password is incorrect', () => {
-        expect(cody.correctPassword('bonez')).to.be.equal(false)
-      })
-    }) // end describe('correctPassword')
-  }) // end describe('instanceMethods')
+    it('returns false if the email is invalid', async () => {
+      try {
+        await cody.validate()
+        throw Error('validation should have failed with invalid email')
+      }
+      catch (err) {
+        expect(err.message).to.contain('validation should have failed with invalid email')
+      }
+    })
+  })
 }) // end describe('User model')

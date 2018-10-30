0f5004f7cc1edf533d281a787e5357dd1747535e
Andrew Ferguson <andrewmf@umich.edu>
2018-10-30 16:11:08 -0500

    add Review model

--- script/seed.js
+++ script/seed.js
@@ -6,7 +6,8 @@ const {
   Product,
   Order,
   OrderItem,
-  Category
+  Category,
+  Review
 } = require('../server/db/models')
 
 async function seed() {
@@ -57,11 +58,40 @@ async function seed() {
     OrderItem.create({orderId: 1, productId: 2, quantity: 5, price: 49.95})
   ])
 
+  const reviews = await Promise.all([
+    Review.create({
+      text:
+        "My son's class required headphones this year for computer lab. He hates wearing anything on his head or ears. These are pretty light but sturdy and comfortable on the ears. They stay put and block outside noise. I tried them and I think I'm going to buy some for myself. I'm very impressed with the price and quality.",
+      rating: 5
+    }),
+    Review.create({
+      text:
+        'I purchased these headphones for my 4 year old grandson. He has had a couple other headphones over the last couple years and they felt flimsy and he refused to wear them. We had an airline flight coming up and thought these would work for him to watch a movie. He left them on the entire time he watched the movie and adjusted easily to fit his head and ears. Only reason for a 4 star instead of 5 star is due to being bulky for travel, however, earbuds would never have worked so I was prepared for bulky.',
+      rating: 4
+    }),
+    Review.create({
+      text:
+        'I bought these headphones for my son to use at school. By the end of his first day using them, the speaker stopped working in one of the ears! I returned them and got my account credited, but it was a huge disappointment and forced my son to go without sound on his iPad for school while I ordered another pair from another company.',
+      rating: 3
+    }),
+    Review.create({
+      text:
+        'Perfect for school! We were looking for a pair of headphones for our seven-year-old daughter‘s second grade class! These sound fantastic, they are in stereo, they are very comfortable and the cord is perfect because it won’t get frayed and spliced like the cheap other ones we had!',
+      rating: 2
+    }),
+    Review.create({
+      text:
+        "I've had them for a few weeks. Went pull them out of my kids iPad and the connector broke. C'mon. Just a few weeks. They were definitely not maltreated or rough-housed with. The metal portion got stuck in the headphone jack. Really annoying to have headphones advertised for kids and they're not made to last. Garbage. Don't buy these.",
+      rating: 1
+    })
+  ])
+
   console.log(`seeded ${users.length} users`)
   console.log(`seeded ${categories.length} categories`)
   console.log(`seeded ${movies.length} movies`)
   console.log(`seeded ${orders.length} orders`)
   console.log(`seeded ${orderItems.length} orders`)
+  console.log(`seeded ${reviews.length} reviews`)
   console.log(`seeded successfully`)
 }
 
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -3,6 +3,7 @@ const Category = require('./category')
 const Product = require('./product')
 const Order = require('./order')
 const OrderItem = require('./orderitem')
+const Review = require('./review')
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -22,6 +23,9 @@ OrderItem.belongsTo(Product)
 Category.belongsToMany(Product, {through: 'tag'})
 Product.belongsToMany(Category, {through: 'tag'})
 
+Review.belongsTo(Product)
+Review.belongsTo(User, {as: 'author'})
+
 /*
  * We'll export all of our models here, so that any time a module needs a model,
  * we can just require it from 'db/models'
@@ -33,5 +37,6 @@ module.exports = {
   Product,
   Order,
   OrderItem,
-  Category
+  Category,
+  Review
 }

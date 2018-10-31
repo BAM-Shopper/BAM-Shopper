77e10b81aaf1dd6d563d95dca53c27526a2c5ab1
Maddie Jennings <madisonlynnjennings@gmail.com>
2018-10-31 13:39:39 -0500

    added navbar component and shell of sidebar component

---
+++ client/components/navbar.spec.js
@@ -0,0 +1,27 @@
+import {expect} from 'chai'
+import React from 'react'
+import enzyme, {shallow} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import navbar from './navbar'
+import SearchBar from './SearchBar'
+import {Link} from 'react-router-dom'
+
+const adapter = new Adapter()
+enzyme.configure({adapter})
+
+describe('Navbar', () => {
+  let wrapper
+  let fakeStore
+
+  beforeEach(() => {
+    // fakeStore = mockStore(initialState)
+
+    wrapper = shallow(<navbar isLoggedIn={false} /> )
+  })
+
+  it('renders SearchBar and Link components', () => {
+    // console.log('SEARCH BAR', wrapper.find(SearchBar).dive())
+    // expect(wrapper.find(Link)).dive().to.have.lengthOf(1)
+  })
+
+})

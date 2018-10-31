7ecaac57e1c32f5bf4694bafe0a61fa3bdef2e00
Maddie Jennings <madisonlynnjennings@gmail.com>
2018-10-31 15:05:39 -0500

    search bar rendering, handleSubmit still incomplete

---
+++ client/components/SearchBar.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Redirect } from 'react-router-dom'
+import {connect} from 'react-redux'
+
+class Search extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      query: ''
+    }
+  }
+
+  handleInputChange = () => {
+    this.setState({
+      query: this.search.value
+    })
+  }
+
+  handleSubmit = evt => {
+    evt.preventDefault()
+    const term = evt.target.value
+    const result = this.props.products.filter(product => {
+      product.name.toLowerCase().includes(term.toLowerCase())
+    })
+    console.log(result)
+  }
+
+  render() {
+    return (
+      <form>
+        <input
+          placeholder="Search movies"
+          ref={input => this.search = input}
+          onChange={this.handleInputChange}
+        />
+        <button
+          type='submit'
+          onSubmit={this.handleSubmit}
+        >Search</button>
+      </form>
+    )
+  }
+}
+
+const mapState = ({products}) => ({products})
+
+export default connect(mapState)(Search)

5aaaaf8b9ec4e5a6f34e3a301d6b24216c7de6e3
Brittany Hill <brit.hill10@gmail.com>
2018-10-31 09:33:54 -0500

    Update singleProduct.js

--- client/store/singleProduct.js
+++ client/store/singleProduct.js
@@ -4,41 +4,28 @@ import history from '../history'
 /**
  * ACTION TYPES
  */
-const GET_PRODUCT = 'GET_PRODUCT'
 const SELECT_PRODUCT = 'SELECT_PRODUCT'
 
 /**
  * INITIAL STATE
  */
 const initialState = {
-    products: [],
-    selectedProduct: {}
+    product: {}
 }
 
 /**
  * ACTION CREATORS
  */
-const getProduct = product => ({ type: GET_PRODUCT, product})
 const selectProduct = product => ({ type: SELECT_PRODUCT, product })
 
 /**
  * THUNK CREATORS
  */
 
-
 export const selectedProduct = () => async dispatch => {
     try {
         const { data } = await axios.get('/api/products/:id')
-        dispatch(selectedProduct(data))
-    } catch (err) {
-        console.error(err)
-    }
-}
-
-export const getProduct = () => async dispatch => {
-    try {
-        const { data } = await axios.get('/api/products/:id')
-        dispatch(selectedProduct(data))
+        dispatch(selectProduct(data))
     } catch (err) {
         console.error(err)
     }
@@ -51,7 +38,7 @@ export default function (state = initialState, action) {
     switch (action.type) {
         case SELECT_PRODUCT:
             return Object.assign({}, state, {
-                selectedProduct: action.product
+                product: action.product
             })
         default:
             return state
